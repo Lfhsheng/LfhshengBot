0125b05c68b42f501d29b22f5df68934a6b10a67
泠风寒声 <lfhsheng@outlook.com>
2022-08-09 12:27:12 +0800

    指令得全部小写

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telebot import *
 from random import randint
 bot = TeleBot("在这填上令牌", parse_mode=None)
-@bot.message_handler(commands=['tossCoin'])
+@bot.message_handler(commands=['tosscoin'])
 def send_coin(message):
     if randint(0,1) == 0:
         bot.reply_to(message,"硬币是反面喵！")

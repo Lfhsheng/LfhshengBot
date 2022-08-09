f90258ca038dfd2f826c6a3578958f0c86874fff
泠风寒声 <lfhsheng@outlook.com>
2022-08-09 12:11:56 +0800

    提交代码和README.py喵！
    咕咕咕，咕出来了😝

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# coinBot
-a bot of Telegram!
+# 关于这个~~破~~机器人
+```python
+python main.py
+```
+记得在`main.py`里填上机器人的令牌😏
---
+++ main.py
@@ -0,0 +1,10 @@
+from telebot import *
+from random import randint
+bot = TeleBot("在这填上令牌", parse_mode=None)
+@bot.message_handler(commands=['tossCoin'])
+def send_coin(message):
+    if randint(0,1) == 0:
+        bot.reply_to(message,"硬币是反面喵！")
+    else:
+        bot.reply_to(message,"硬币是正面喵！")
+bot.infinity_polling()

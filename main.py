8be3420aa25d0af8895f9b6d6797161d76360c50
泠风寒声 <lfhsheng@outlook.com>
2022-08-10 09:33:16 +0800

    添加/ping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telebot import *
 from random import randint
-token = "在这填上令牌"
+token = "5588904129:AAHmOmeN2hfllUYC3jU6ZPcoxkZD40QKVOk"
+pingList = ["喵喵喵","我还活着……","呜呜呜","挠挠挠","伸爪ing"]
 bot = TeleBot(token, parse_mode=None)
 @bot.message_handler(commands=["tosscoin"])
 def send_coin(message):
@@ -12,6 +13,9 @@ def send_coin(message):
 @bot.message_handler(commands=["meow"])
 def send_meow(message):
     bot.reply_to(message,"喵")
+@bot.message_handler(commands=["ping"])
+def send_ping(message):
+    bot.reply_to(message,pingList[randint(0,len(pingList)-1)])
 #@bot.message_handler(func=lambda message: True) 
 #def echo_all(message):
 #    bot.reply_to(message,"喵！")

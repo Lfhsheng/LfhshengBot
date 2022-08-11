023c05b155a407c8e4415cd1adcd5e0f37a59a99
泠风寒声 <lfhsheng@outlook.com>
2022-08-11 11:08:56 +0800

    删除自建一言列表，改用api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from telebot import *
 from random import randint
+from json import loads
+from requests import get
 token = "在这填上令牌"
 pingList = ["喵喵喵","我还活着……","呜呜呜","挠挠挠","伸爪ing"]
-wordList = ["人生无常，大肠包小肠","喵你喵！","我在這裡下詛咒，病夫在30歲以前一定會結婚","有時間催促女裝不如寫一兩篇稿子"]
 keyWordList = [
     ["qwq","awa"],
     ["喵","喵！"],
@@ -12,6 +13,10 @@ keyWordList = [
     ["泠风寒声","泠风寒声翻车车~"],
     ["冷风寒声","是泠风寒声！"]
     ]
+def word():
+    jsonWord = get("https://v1.hitokoto.cn/")
+    text = loads(jsonWord.text)
+    return text["hitokoto"]+"--"+text["from"]
 bot = TeleBot(token, parse_mode=None)
 @bot.message_handler(commands=["tosscoin"])
 def send_coin(message):
@@ -22,17 +27,20 @@ def send_coin(message):
         bot.reply_to(message,"硬币是正面喵！")
 @bot.message_handler(commands=["meow"])
 def send_meow(message):
+    print("喵")
     bot.reply_to(message,"喵")
 @bot.message_handler(commands=["ping"])
 def send_ping(message):
+    print("有人在ping喵")
     bot.reply_to(message,pingList[randint(0,len(pingList)-1)])
 @bot.message_handler(commands=["word"])
 def send_word(message):
-    bot.reply_to(message,wordList[randint(0,len(wordList)-1)])
+    print("有人在看一言喵")
+    bot.reply_to(message,word())
 @bot.message_handler(func=lambda message: True)
 def checkKeyWord(message):
-    print(message.text)
     for listNum in range(0,len(keyWordList)-1):
         if keyWordList[listNum][0] == message.text:
             bot.reply_to(message,keyWordList[listNum][1])
+            print("关键字已回复")
 bot.infinity_polling()

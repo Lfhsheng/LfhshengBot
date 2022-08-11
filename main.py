e943374b850bdf2f9284d5994a55aa67ba5d4e06
泠风寒声 <lfhsheng@outlook.com>
2022-08-11 09:39:09 +0800

    修改关键词检测

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from random import randint
 token = "在这填上令牌"
 pingList = ["喵喵喵","我还活着……","呜呜呜","挠挠挠","伸爪ing"]
 wordList = ["人生无常，大肠包小肠","喵你喵！","我在這裡下詛咒，病夫在30歲以前一定會結婚","有時間催促女裝不如寫一兩篇稿子"]
+keyWordList = [
+    ["qwq","awa"],
+    ["喵","喵！"],
+    ["紫砂","不要！"],
+    ["Emo酱","Emo酱主义万岁！"],
+    ["咕谷酱","咕咕咕！"],
+    ["泠风寒声","泠风寒声翻车车~"],
+    ["冷风寒声","是泠风寒声！"]
+    ]
 bot = TeleBot(token, parse_mode=None)
 @bot.message_handler(commands=["tosscoin"])
 def send_coin(message):
@@ -23,14 +32,7 @@ def send_word(message):
 @bot.message_handler(func=lambda message: True)
 def checkKeyWord(message):
     print(message.text)
-    if "qwq" in message.text:
-        bot.reply_to(message,"awa")
-    elif "喵" in message.text:
-        bot.reply_to(message,"喵！")
-    elif "紫砂" in message.text:
-        bot.reply_to(message,"不要！")
-    elif "Emo酱" in message.text:
-        bot.reply_to(message,"Emo酱主义万岁！")
-    elif "咕谷酱" in message.text:
-        bot.reply_to(message,"咕咕咕！")
+    for listNum in range(0,len(keyWordList)-1):
+        if keyWordList[listNum][0] == message.text:
+            bot.reply_to(message,keyWordList[listNum][1])
 bot.infinity_polling()
